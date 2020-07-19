9ab4c3ed3b5c6e9985a4e7907957bf706849399a
anj180190 <anj180190@gmail.com>
2020-07-19 19:39:45 +0530

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  pathPrefix: "/ziva-it",
+  /* Your site config here */
+  plugins: [],
+}
